0ae2b78ac20c3355ea686cb01e269af71c2bf1b0
purebluesong <mousecatchcat@yeah.net>
2016-06-22 19:05:32 +0800

    update

---
+++ app/src/main/java/com/sprout/wi/stoflo/Global.java
@@ -0,0 +1,10 @@
+package com.sprout.wi.stoflo;
+
+import com.avos.avoscloud.AVUser;
+
+/**
+ * Created by purebluesong on 2016/6/21.
+ */
+public class Global {
+    public static AVUser user = null;
+}
---
+++ app/src/main/java/com/sprout/wi/stoflo/StoFloActivity.java
@@ -0,0 +1,20 @@
+package com.sprout.wi.stoflo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by purebluesong on 2016/6/21.
+ */
+public class StoFloActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stoflo);
+        TextView tv = (TextView) findViewById(R.id.test_text);
+        tv.setText("login"+getIntent().getBooleanExtra("login",false));
+
+    }
+}
